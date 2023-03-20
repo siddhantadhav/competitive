97f03c3a3db40271f6a782a6344bddea8c49a31e
siddhantadhav <isiddhantadhav@gmail.com>
2023-03-20 21:02:18 +0530

    setup done for dsa

--- Data Structures/Array/array.cpp
+++ Data Structures/Array/array.cpp
@@ -6,7 +6,20 @@ int main() {
 	freopen("../../input.txt", "r", stdin);
 	freopen("../../output.txt", "w", stdout);
 
-	cout << "Hello World" << endl;
+	// Array Creation
+
+	int n;
+	cin >> n;
+
+	int arr[n];
+
+	for (int i = 0; i < n; i++) {
+		cin >> arr[i];
+	}
+
+	for (int i = 0; i < n; ++i) {
+		cout << arr[i] << "\n";
+	}
 
 	fprintf(stderr, "\n>> Runtime: %.10fs\n", (double) (clock() - tStart) / CLOCKS_PER_SEC);
 
