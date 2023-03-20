7ff052189aa7c5687611fc206fa9ccf07db9f023
siddhantadhav <isiddhantadhav@gmail.com>
2023-03-20 20:48:25 +0530

    first commit

---
+++ Data Structures/Array/array.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	clock_t tStart = clock();
+	freopen("../../input.txt", "r", stdin);
+	freopen("../../output.txt", "w", stdout);
+
+	cout << "Hello World" << endl;
+
+	fprintf(stderr, "\n>> Runtime: %.10fs\n", (double) (clock() - tStart) / CLOCKS_PER_SEC);
+
+	return 0;
+}
---
+++ template.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	#ifndef ONLINE_JUDGE
+		clock_t tStart = clock();
+		freopen("input.txt", "r", stdin);
+		freopen("output.txt", "w", stdout);
+	#endif
+
+	
+
+	#ifndef ONLINE_JUDGE
+		fprintf(stderr, "\n>> Runtime: %.10fs\n", (double) (clock() - tStart) / CLOCKS_PER_SEC);
+	#endif
+
+	return 0;
+}
