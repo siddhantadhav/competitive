caf01a0225a81413b04d8d72e508c2171b111b9a
siddhantadhav <isiddhantadhav@gmail.com>
2024-03-07 18:50:12 +0530

    template ready

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long int;
+#define ld long double;
+
+int main() {
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    int t;
+    cin >> t;
+
+    while (t--) {
+        cout << "Hello World" << "\n";
+    }
+    
+    return 0;
+}
